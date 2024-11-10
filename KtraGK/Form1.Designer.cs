0939de05d1636aa009b9af3db5e2db6a2de0ab52
An0411 <anbunnx@gmail.com>
2024-11-10 15:54:28 +0700

    init project

---
+++ KtraGK/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace KtraGK
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewStudents = new System.Windows.Forms.DataGridView();
+            this.studentIDColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.classColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStudentID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAddStudent = new System.Windows.Forms.Button();
+            this.btnDeleteStudent = new System.Windows.Forms.Button();
+            this.btnSearchStudent = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewStudents
+            // 
+            this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudents.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.studentIDColumn,
+            this.nameColumn,
+            this.classColumn});
+            this.dataGridViewStudents.Location = new System.Drawing.Point(85, 12);
+            this.dataGridViewStudents.Name = "dataGridViewStudents";
+            this.dataGridViewStudents.RowHeadersWidth = 51;
+            this.dataGridViewStudents.RowTemplate.Height = 24;
+            this.dataGridViewStudents.Size = new System.Drawing.Size(427, 150);
+            this.dataGridViewStudents.TabIndex = 0;
+            // 
+            // studentIDColumn
+            // 
+            this.studentIDColumn.HeaderText = "Mã sinh viên";
+            this.studentIDColumn.MinimumWidth = 6;
+            this.studentIDColumn.Name = "studentIDColumn";
+            this.studentIDColumn.Width = 125;
+            // 
+            // nameColumn
+            // 
+            this.nameColumn.HeaderText = "Họ tên";
+            this.nameColumn.MinimumWidth = 6;
+            this.nameColumn.Name = "nameColumn";
+            this.nameColumn.Width = 125;
+            // 
+            // classColumn
+            // 
+            this.classColumn.HeaderText = "Lớp";
+            this.classColumn.MinimumWidth = 6;
+            this.classColumn.Name = "classColumn";
+            this.classColumn.Width = 125;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã sinh viên";
+            // 
+            // txtStudentID
+            // 
+            this.txtStudentID.Location = new System.Drawing.Point(169, 186);
+            this.txtStudentID.Name = "txtStudentID";
+            this.txtStudentID.Size = new System.Drawing.Size(184, 22);
+            this.txtStudentID.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(169, 215);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(184, 22);
+            this.txtName.TabIndex = 3;
+            this.txtName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtClass
+            // 
+            this.txtClass.Location = new System.Drawing.Point(169, 244);
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(184, 22);
+            this.txtClass.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Họ tên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Lớp";
+            // 
+            // btnAddStudent
+            // 
+            this.btnAddStudent.Location = new System.Drawing.Point(85, 290);
+            this.btnAddStudent.Name = "btnAddStudent";
+            this.btnAddStudent.Size = new System.Drawing.Size(142, 23);
+            this.btnAddStudent.TabIndex = 7;
+            this.btnAddStudent.Text = "Thêm sinh viên";
+            this.btnAddStudent.UseVisualStyleBackColor = true;
+            this.btnAddStudent.Click += new System.EventHandler(this.btnAddStudent_Click);
+            // 
+            // btnDeleteStudent
+            // 
+            this.btnDeleteStudent.Location = new System.Drawing.Point(85, 336);
+            this.btnDeleteStudent.Name = "btnDeleteStudent";
+            this.btnDeleteStudent.Size = new System.Drawing.Size(142, 23);
+            this.btnDeleteStudent.TabIndex = 8;
+            this.btnDeleteStudent.Text = "Xóa sinh viên";
+            this.btnDeleteStudent.UseVisualStyleBackColor = true;
+            this.btnDeleteStudent.Click += new System.EventHandler(this.btnDeleteStudent_Click);
+            // 
+            // btnSearchStudent
+            // 
+            this.btnSearchStudent.Location = new System.Drawing.Point(429, 185);
+            this.btnSearchStudent.Name = "btnSearchStudent";
+            this.btnSearchStudent.Size = new System.Drawing.Size(138, 23);
+            this.btnSearchStudent.TabIndex = 9;
+            this.btnSearchStudent.Text = "Tìm kiếm sinh viên";
+            this.btnSearchStudent.UseVisualStyleBackColor = true;
+            this.btnSearchStudent.Click += new System.EventHandler(this.btnSearchStudent_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(573, 186);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(183, 22);
+            this.txtSearch.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearchStudent);
+            this.Controls.Add(this.btnDeleteStudent);
+            this.Controls.Add(this.btnAddStudent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtClass);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtStudentID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewStudents);
+            this.Name = "Form1";
+            this.Text = "Quản lý Danh sách Sinh viên";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewStudents;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentIDColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn classColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStudentID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAddStudent;
+        private System.Windows.Forms.Button btnDeleteStudent;
+        private System.Windows.Forms.Button btnSearchStudent;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
+
---
+++ KtraGK/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KtraGK
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddStudent_Click(object sender, EventArgs e)
+        {
+            string studentID = txtStudentID.Text;
+            string name = txtName.Text;
+            string studentClass = txtClass.Text;
+
+            
+            if (string.IsNullOrWhiteSpace(studentID) || string.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show("Mã sinh viên và Họ tên không được để trống.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            
+            foreach (DataGridViewRow row in dataGridViewStudents.Rows)
+            {
+                if (row.Cells["studentIDColumn"].Value?.ToString() == studentID)
+                {
+                    MessageBox.Show("Mã sinh viên đã tồn tại.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+           
+            dataGridViewStudents.Rows.Add(studentID, name, studentClass);
+
+            
+            txtStudentID.Clear();
+            txtName.Clear();
+            txtClass.Clear();
+        }
+
+        private void btnDeleteStudent_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewStudents.SelectedRows.Count > 0)
+            {
+                dataGridViewStudents.Rows.RemoveAt(dataGridViewStudents.SelectedRows[0].Index);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một sinh viên để xóa.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnSearchStudent_Click(object sender, EventArgs e)
+        {
+            string keyword = txtSearch.Text.ToLower();
+
+            
+            foreach (DataGridViewRow row in dataGridViewStudents.Rows)
+            {
+                row.Visible = true;
+            }
+
+            
+            foreach (DataGridViewRow row in dataGridViewStudents.Rows)
+            {
+                if (row.Cells["nameColumn"].Value != null && !row.Cells["nameColumn"].Value.ToString().ToLower().Contains(keyword))
+                {
+                    row.Visible = false;
+                }
+            }
+        }
+    }
+}
